417a47abefe73a62df8825731f2d8c6b517b9eac
m001png <mongkonchai4412@gmail.com>
2022-02-11 15:40:14 +0700

    init cypress

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+describe('My First Test Suite', () => {
+  it('My FirstTest case', () => {
+    cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+
+    cy.get('.product').should('have.length', 5)
+    cy.get('.product:visible').should('have.length', 4)
+
+    cy.get('.products').as('productLocator')
+    cy.get('@productLocator').find('.product').should('have.length', 4)
+
+    cy.get(':nth-child(3) > .product-action > button').click()
+    cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click()
+
+
+    cy.get('@productLocator').find('.product').each(($el, index, $list) => {
+      const textVeg = $el.find('h4.product-name').text()
+
+      if (textVeg.includes('Cashews')) {
+        cy.wrap($el).find('button').click()
+      }
+    })
+
+    cy.get('.brand').should('have.text', 'GREENKART')
+
+    cy.get('.brand').then((logoElement) => {
+      cy.log(logoElement.text())
+    })
+  })
+})
